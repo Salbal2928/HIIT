aa5a462da37aa8a38ac4070add1f47c2908b03f4
unknown <abdulrahimsalihu823@gmail.com>
2023-02-06 12:23:49 +0100

    message

---
+++ Functions/calculators.py
@@ -0,0 +1,48 @@
+
+def main():
+    x = int(input("What is x: "))
+    y = int(input("what is y: "))
+    added(x, y)
+    sub(x, y)
+    multiply(x, y)
+    modulus(x, y)
+    divide(x, y)
+    power(x,y)
+
+
+def added(x, y):
+    print(x)
+    print(y)
+    print(x+y)
+
+def sub(x, y):
+    print(x)
+    print(y)
+    print(x-y)
+
+def divide(x, y):
+    print(x)
+    print(y)
+    print(x/y)    
+
+def multiply(x, y):
+    print(x)
+    print(y)
+    print(x*y)
+
+def modulus(x, y):
+    print(x)
+    print(y)
+    print(x%y) 
+
+def power(x, y):
+    print(x)
+    print(y)
+    print(x**y)
+main()
+
+
+
+
+
+
---
+++ Functions/hello_2.py
@@ -0,0 +1,9 @@
+name = input("what is your name: ")
+
+
+def print_name(fname, lname):
+    "print names passed into variables name"
+    print(f"Your full name is {fname} {lname}")
+
+print_name("salbal", "Abdull")
+print(name)
